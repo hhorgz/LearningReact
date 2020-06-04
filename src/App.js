5dd80ff243ec00667a5188bd78750db969354bb1
hhorgz <hanns.guzman@gmail.com>
2020-06-03 21:01:50 -0600

    Section 3 Understanding the base features & syntax

--- src/App.js
+++ src/App.js
@@ -3,18 +3,69 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
-        <button>Switch Name</button>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
-      </div>
-    );
-  }
+	
+	state = {
+		persons: [
+			{ name: 'Max', age: 28 },
+			{ name: 'Manu', age: 29 },
+			{ name: 'Stephanie', age: 26}
+		],
+		otherState: 'some other value'
+	}
+
+	switchNameHandler = (newName) => {
+		// console.log('Was clicked!');
+		// DONT DO THIS: this.state.persons[0] = 'Maximiliam';
+		this.setState({
+			persons: [
+				{ name: newName, age: 28 },
+				{ name: 'Manu', age: 29 },
+				{ name: 'Stephanie', age: 27}
+			]
+		});
+	}
+
+	nameChangeHandler = (event) => {
+		this.setState({
+			persons: [
+				{ name: 'Max', age: 28 },
+				{ name: event.target.value, age: 29 },
+				{ name: 'Stephanie', age: 27}
+			]
+		});
+	}
+
+	render() {
+
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer'
+		}
+
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working</p>
+				<button 
+					style={style}
+					onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+				<Person 
+					name={this.state.persons[0].name} 
+					age={this.state.persons[0].age}/>
+				<Person 
+					name={this.state.persons[1].name} 
+					age={this.state.persons[1].age}
+					click={this.switchNameHandler.bind(this, 'Max!')}
+					changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
+				<Person 
+					name={this.state.persons[2].name} 
+					age={this.state.persons[2].age}/>
+			</div>
+		);
+	}
 }
 
 export default App;
